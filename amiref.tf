244a7a3d4cac2d05ae864b9e54798a95a2bb9635
Ion Drugalea <iondrugaela20@gmail.com>
2019-02-26 09:55:24 +0000

    remove .tfvars file

---
+++ amiref.tf
@@ -0,0 +1,22 @@
+data "aws_ami" "jenkins-master"{
+most_recent = true
+owners = ["self"]
+filter {
+    name  = "name"
+    values = [" jenkins-master"]
+   }
+}
+
+
+data "aws_ami" "jenkins-slave"{
+
+most_recent = "true"
+owners = ["self"]
+
+filter {
+name = "name"
+values  = ["jenkins-slave"]
+
+}
+
+}
